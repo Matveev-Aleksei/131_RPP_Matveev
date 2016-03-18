6f74a6d6f238d466e4581efbf404b1d6999f0dbd
Aleksei-Matveev <A.matveev1995@mail.ru>
2016-03-18 11:24:04 +0300

    Работа

--- RPP лаб работа №1/Program.cs
+++ RPP лаб работа №1/Program.cs
@@ -10,96 +10,96 @@ namespace RPP_лаб_работа__1
     {
         static void Main(string[] args)
         {
-            // Объявление переменных (1)
-            int x;
-            int y;
-            int i;
-            int f = 0;
-            int t;
-            int max = 4;
-            Random rnd1 = new Random();
-            int number = rnd1.Next(max);
-            Random rnd12 = new Random();
-            int number1 = rnd12.Next(max);
+            //// Объявление переменных (1)
+            //int x;
+            //int y;
+            //int i;
+            //int f = 0;
+            //int t;
+            //int max = 4;
+            //Random rnd1 = new Random();
+            //int number = rnd1.Next(max);
+            //Random rnd12 = new Random();
+            //int number1 = rnd12.Next(max);
 
-            int b = 0;
+            //int b = 0;
 
-            // Присваивание значений (2)
-            x = 7;
-            y = 9;
+            //// Присваивание значений (2)
+            //x = 7;
+            //y = 9;
 
-            // Объявление и инициализация переменной z (3)
-            int z = y + x;
+            //// Объявление и инициализация переменной z (3)
+            //int z = y + x;
 
-            // Вывод на экран z (4)
-            Console.WriteLine(z);
+            //// Вывод на экран z (4)
+            //Console.WriteLine(z);
 
-            // Проверка и выполнение условий (5)
-            if (z > 0)
-                {
-                    Console.WriteLine("z положительное ");
-                }
-                else
-                {
-                    Console.WriteLine("z отрицательнлое ");
-                }
+            //// Проверка и выполнение условий (5)
+            //if (z > 0)
+            //    {
+            //        Console.WriteLine("z положительное ");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("z отрицательнлое ");
+            //    }
 
-            if (z % 2 == 0)
-                {
-                    Console.WriteLine("z четное");
-                }
-                else 
-                {
-                    Console.WriteLine("z не четное");
-                }
-            if (z % 5 == 0)
-                {
-                    Console.WriteLine("Кратно 5 ");
-                }
-                else
-                {
-                    Console.WriteLine("Не кратно 5 ");
+            //if (z % 2 == 0)
+            //    {
+            //        Console.WriteLine("z четное");
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("z не четное");
+            //    }
+            //if (z % 5 == 0)
+            //    {
+            //        Console.WriteLine("Кратно 5 ");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Не кратно 5 ");
 
-                }
+            //    }
             
-            // Использование оператора goto (7)
-            point:
-                if (z > 0)
-                {
-                    z--;
-                    Console.WriteLine(z);
-                    goto point;
-                }
+            //// Использование оператора goto (7)
+            //point:
+            //    if (z > 0)
+            //    {
+            //        z--;
+            //        Console.WriteLine(z);
+            //        goto point;
+            //    }
 
-            // Использование оператора goto (7)
-            point1:
-            {
-                z--;
-                Console.WriteLine(z);
-            }
-            if (z > 0)
-            goto point1;
+            //// Использование оператора goto (7)
+            //point1:
+            //{
+            //    z--;
+            //    Console.WriteLine(z);
+            //}
+            //if (z > 0)
+            //goto point1;
             
 
-            // Использование оператора while (9)
-            while (z > 0)
-            {
-                z--;
-                Console.WriteLine(z);
-            }
-            // Использование оператора do while (9)
-            do
-            {
-                  z--;
-                Console.WriteLine(z);
-            }
-            while(z > 0);
+            //// Использование оператора while (9)
+            //while (z > 0)
+            //{
+            //    z--;
+            //    Console.WriteLine(z);
+            //}
+            //// Использование оператора do while (9)
+            //do
+            //{
+            //      z--;
+            //    Console.WriteLine(z);
+            //}
+            //while(z > 0);
 
-            // Использование оператора for (10)
-            for (i = z; z > 0; z--)
-            {
-                Console.WriteLine(z);
-            }
+            //// Использование оператора for (10)
+            //for (i = z; z > 0; z--)
+            //{
+            //    Console.WriteLine(z);
+            //}
 
             // Угадай число 
            // do
@@ -142,46 +142,6 @@ namespace RPP_лаб_работа__1
            // {
            //     Console.WriteLine("Можете идти гулять");
            // }
-
-            Console.WriteLine("Загадайте число от 0 до 4 ");
-            Console.ReadKey();
-
-            do
-            {
-                Random rnd11 = new Random();
-                int number2 = rnd1.Next(max);
-
-                Console.WriteLine("Ваше число " + number2 + " ? ");
-                ConsoleKeyInfo e = Console.ReadKey();
-
-                switch (e.Key)
-                {
-                    case ConsoleKey.F1:
-                        {
-                            Console.WriteLine(" - Загаданное число больше");
-                        }
-                        break;
-                    case ConsoleKey.F2:
-                        {
-                            Console.WriteLine(" - Загаданное число меньше");
-                        }
-                        break;
-                    case ConsoleKey.F3:
-                        {
-                            Console.WriteLine("Поздравляем... Вы угадали! ");
-                            Console.ReadKey();
-                            return;
-                        }
-                        break;
-                }
-                f++;
-            }
-            while (f < 3);
-            {
-                Console.WriteLine("Я не угадал... Попробуем еще ? ");
-            }
-            
-            // Таблица 
 
             //количество итераций внешнего цикла
 
--- Задание  3/Program.cs
+++ Задание  3/Program.cs
@@ -10,130 +10,195 @@ namespace Задание__3
     {
         static void Main(string[] args)
         {
-            //Задание 1 и 2
-            int[] sample;
-            sample = new int[100];
+           
 
-            for (int i = 0; i < sample.Length; i++)
+            ////Задание 1 и 2
+            //int[] sample;
+            //sample = new int[100];
+
+            //for (int i = 0; i < sample.Length; i++)
+            //{
+            //    sample[i] = i;
+            //}
+            //for (int i = 0; i < sample.Length; i++)
+            //{
+            //    Console.WriteLine(sample[i]);
+            //}
+
+            ////Задание 3
+            //int[] sample1;
+            //sample1 = new int[200];
+
+            //for (int i = 100; i < sample1.Length; i++)
+            //{
+            //    sample1[i] = i;
+            //}
+            //for (int i = 100; i < sample1.Length; i++)
+            //{
+            //    Console.WriteLine(sample1[i]);
+            //}
+
+            ////Задание 4
+            //int[] sample2;
+            //sample2 = new int[5];
+
+            //for (int i = -5; i < sample2.Length; i++)
+            //{
+            //    sample2[i] = i;
+            //}
+            //for (int i = -5; i < sample2.Length; i++)
+            //{
+            //    Console.WriteLine(sample2[i]);
+            //}
+
+            ////Задание 5
+            //int[] sample5;
+            //sample5 = new int[200];
+
+            //for (int i = 0; i < sample5.Length; i++)
+            //{
+            //    sample5[i] = i;
+            //}
+            //for (int i = 0; i < sample5.Length; i++)
+            //{
+            //    if (sample5[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(sample5[i]);
+            //    }
+            //}
+
+            ////Задание 6
+            //int[] sample6;
+            //sample6 = new int[402];
+
+            //for (int i = 100; i < sample6.Length; i++)
+            //{
+            //    sample6[i] = i;
+            //}
+            //for (int i = 100; i < sample6.Length; i++)
+            //{
+            //    if (sample6[i] % 2 == 1)
+            //    {
+            //        Console.WriteLine(sample6[i]);
+            //    }
+            //}
+
+
+            ////Задание 7
+            //double[] sample7;
+            //sample7 = new double[100];
+
+            //for (int i = 0; i < sample7.Length; i++)
+            //{
+            //    sample7[i] = i;
+            //}
+            //for (int i = 0; i < sample7.Length; i++)
+            //{
+            //    Console.WriteLine(sample7[i]);
+            //}
+
+            ////Задание 8
+            //int[] sample8;
+            //sample8 = new int[100];
+
+            //for (int i = 0; i < sample8.Length; i++)
+            //{
+            //    sample8[i] = i;
+            //    Console.WriteLine("0");
+            //    Console.WriteLine("1");
+            //}
+
+            ////Задание 9
+            //int[] sample9;
+            //sample9 = new int[100];
+
+            //for (int i = 0; i < sample9.Length; i++)
+            //{
+            //    sample9[i] = i;
+            //    Console.WriteLine("1");
+            //    Console.WriteLine("0");
+            //}
+
+            ////Задание 6
+            //int[] sample10;
+            //sample10 = new int[200];
+            
+            //    for (int i = 100; i < sample10.Length; i--)
+            //    {
+            //        if (i >= 0)
+            //        {
+            //            sample10[i] = i;
+
+            //            if (sample10[i] % 3 == 0)
+            //            {
+            //                Console.WriteLine(sample10[i]);
+            //            }
+
+            //        }
+            //    }
+            
+            
+            ////Задание 26
+            //int max = 50;
+            //Random rnd = new Random();
+            //int[] m = new int[10];
+            //int s = 0;
+            //for (int i = 0; i < m.Length; i++)
+            //{
+            //    m[i] = rnd.Next(max);
+
+            //    if (m[i] % 2 == 1)
+            //    {
+            //        s = s + m[i];
+            //    }
+            //}
+            //for (int i = 0; i < m.Length; i++)
+            //{
+            //    Console.WriteLine(m[i]);
+            //}
+
+            //Console.WriteLine("s = " + s);
+            //Console.ReadKey();
+
+            //Задание 46
+            int max = 10;
+            Random rnd = new Random();
+            int s = 0;
+            int[] m = new int[10];
+            for (int i = 0; i < m.Length; i++)
             {
-                sample[i] = i;
+                m[i] = rnd.Next(-max, max);
             }
-            for (int i = 0; i < sample.Length; i++)
+            for (int i = 0; i < m.Length; i++)
             {
-                Console.WriteLine(sample[i]);
+                Console.WriteLine(m[i]);
             }
 
-            //Задание 3
-            int[] sample1;
-            sample1 = new int[200];
-
-            for (int i = 100; i < sample1.Length; i++)
+            for (int i = 0; i < m.Length; i++)
             {
-                sample1[i] = i;
-            }
-            for (int i = 100; i < sample1.Length; i++)
-            {
-                Console.WriteLine(sample1[i]);
-            }
-
-            //Задание 4
-            int[] sample2;
-            sample2 = new int[5];
-
-            for (int i = -5; i < sample2.Length; i++)
-            {
-                sample2[i] = i;
-            }
-            for (int i = -5; i < sample2.Length; i++)
-            {
-                Console.WriteLine(sample2[i]);
-            }
-
-            //Задание 5
-            int[] sample5;
-            sample5 = new int[200];
-
-            for (int i = 0; i < sample5.Length; i++)
-            {
-                sample5[i] = i;
-            }
-            for (int i = 0; i < sample5.Length; i++)
-            {
-                if (sample5[i] % 2 == 0)
+                do
                 {
-                    Console.WriteLine(sample5[i]);
+                    if (m[i] > 0)
+                    {
+                        Console.WriteLine("Число положительное ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число отрицательное ");
+                    }
+                    i++;
                 }
+
+
+                while (i < 10);
             }
 
-            //Задание 6
-            int[] sample6;
-            sample6 = new int[402];
-
-            for (int i = 100; i < sample6.Length; i++)
-            {
-                sample6[i] = i;
-            }
-            for (int i = 100; i < sample6.Length; i++)
-            {
-                if (sample6[i] % 2 == 1)
-                {
-                    Console.WriteLine(sample6[i]);
-                }
-            }
-
-
-            //Задание 7
-            double[] sample7;
-            sample7 = new double[100];
-
-            for (int i = 0; i < sample7.Length; i++)
-            {
-                sample7[i] = i;
-            }
-            for (int i = 0; i < sample7.Length; i++)
-            {
-                Console.WriteLine(sample7[i]);
-            }
-
-            //Задание 8
-            int[] sample8;
-            sample8 = new int[100];
-
-            for (int i = 0; i < sample8.Length; i++)
-            {
-                sample8[i] = i;
-                Console.WriteLine("0");
-                Console.WriteLine("1");
-            }
-
-            //Задание 9
-            int[] sample9;
-            sample9 = new int[100];
-
-            for (int i = 0; i < sample9.Length; i++)
-            {
-                sample9[i] = i;
-                Console.WriteLine("1");
-                Console.WriteLine("0");
-            }
-
-            //Задание 10
-            int[] sample10;
-            sample10 = new int[200];
-
-            for (int i = 100; i < sample10.Length; i--)
-            {
-                sample10[i] = i;
-                if (sample10[i] % 3 == 0)
-                {
-                    Console.WriteLine(sample10[i]);
-                }
-            }
-
-
-
-
+            
             Console.ReadKey();
+
+
+
+                
         }
     }
 }
